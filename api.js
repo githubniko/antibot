2327b93e9ee0042e881e0c77bea166d58c3c3c9e
Egor N <egor@intrell.ru>
2025-03-14 01:46:20 +0300

    Устранены проблемы с совместимостью с некоторыми старыми браузерами, такими как IE11

--- api.js
+++ api.js
@@ -9,24 +9,25 @@ const blockVerifying = document.getElementById("verifying");
 const blockFail = document.getElementById("fail");
 let KEY_ID = "";
 
+/*
 function isWebWorkerSupported() {
-    return typeof Worker !== 'undefined';
+	return typeof Worker !== 'undefined';
 }
 
 let workerCount = 0;
 function createWorker(countEnd) {
 	countEnd = (countEnd == undefined) ? 10:countEnd;
-    try {
-        const worker = new Worker('benchmark.js');
-        worker.postMessage('start');
-        workerCount++;
-        console.log(`Worker ${workerCount} started`);
-        if(countEnd < workerCount) 
+	try {
+		const worker = new Worker('benchmark.js');
+		worker.postMessage('start');
+		workerCount++;
+		console.log(`Worker ${workerCount} started`);
+		if(countEnd < workerCount) 
 			createWorker();
-    } catch (error) {
-        console.error(`Failed to create Worker ${workerCount + 1}:`, error);
-        console.log(`Maximum workers supported: ${workerCount}`);
-    }
+	} catch (error) {
+		console.error(`Failed to create Worker ${workerCount + 1}:`, error);
+		console.log(`Maximum workers supported: ${workerCount}`);
+	}
 }
 
 function startBanchmark() {
@@ -36,68 +37,73 @@ function startBanchmark() {
 		console.error('Web Workers are not supported in this browser.');
 	}
 }
+*/
 
-function checkBot(func) {
-	fetch(HTTP_ANTIBOT_PATH + 'check_bot.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            screenWidth: window.screen.width,
-            screenHeight: window.screen.height,
-            pixelRatio: window.devicePixelRatio || 1,
-			referrer: document.referrer,
-			mainFrame: window.top === window.self,
-			func: func == undefined ? 'check':func,
-			keyID: KEY_ID,
-        })
-    })
-	.then(response => {
-		if (!response.ok) {
-			throw new Error('Network response was not ok ' + response.statusText);
-		}
-		return response.json();
-	})
-	.then(data => {
-		if(data.status == 'captcha') {
-			KEY_ID = data.keyID;
-			displayCaptcha();
-		}
-		else if(data.status == 'allow') {
-			form.style.display = "none";
-			lspinner.style.display = "none";
-			blockHTTPSecurity.style.display = "none";
-			blockSuccessful.style.display = "block";
-			setTimeout(() => { window.location.href = window.location.href; }, 1000);
-		}
-		else if(data.status == 'block') {
-			setTimeout(() => { window.location.href = window.location.href; }, 1000);
-		}
-	})
-	.catch(error => {
-		console.log(error);
-	});
-
+function refresh() {
+	window.location.href = window.location.href;
 }
 
-function displayCaptcha()
-{
+function checkBot(func) {
+	var xhr = new XMLHttpRequest();
+
+	var data = JSON.stringify({ // Данные для отправки
+		screenWidth: window.screen.width,
+		screenHeight: window.screen.height,
+		pixelRatio: window.devicePixelRatio || 1,
+		referrer: document.referrer,
+		mainFrame: window.top === window.self,
+		func: func == undefined ? 'check' : func,
+		keyID: KEY_ID,
+	});
+
+	xhr.open('POST', HTTP_ANTIBOT_PATH + 'check_bot.php', true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+
+	xhr.onload = function () {
+		if (xhr.status >= 200 && xhr.status < 300) {
+			var data = JSON.parse(xhr.responseText);
+			if (data.status == 'captcha') {
+				KEY_ID = data.keyID;
+				displayCaptcha();
+			}
+			else if (data.status == 'allow') {
+				form.style.display = "none";
+				lspinner.style.display = "none";
+				blockHTTPSecurity.style.display = "none";
+				blockSuccessful.style.display = "block";
+				setTimeout(refresh, 1000);
+			}
+			else if (data.status == 'block') {
+				setTimeout(refresh, 1000);
+			}
+		} else {
+			console.error('Request failed with status:', xhr.status, xhr.statusText);
+		}
+	};
+
+	xhr.onerror = function () {
+		console.error('Network error occurred');
+	};
+
+	xhr.send(data);
+}
+
+function displayCaptcha() {
 	head2.textContent = "Подтвердите, что вы человек, выполнив указанное действие.";
 	form.style.display = "grid";
 	lspinner.style.display = "none";
 	blockHTTPSecurity.style.display = "none";
-	
-	input.addEventListener('click', function(event) { 
+
+	input.addEventListener('click', function (event) {
 		if (this.checked) {
 			blockInput.style.display = "none";
 			blockVerifying.style.display = "";
 			checkBot('set-marker');
 		}
 	});
-	window.onbeforeunload = function(e) {
+	window.onbeforeunload = function (e) {
 		checkBot('win-close');
-	  };
+	};
 }
 
 checkBot();
