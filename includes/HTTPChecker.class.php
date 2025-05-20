edfeb1387a1a1cfbccbe801d96264cee5dba0ef9
Egor N <egor@intrell.ru>
2025-05-21 02:11:23 +0300

    Доработки

--- includes/HTTPChecker.class.php
+++ includes/HTTPChecker.class.php
@@ -15,11 +15,7 @@ class HTTPChecker
     {
         $this->Config = $config;
 
-<<<<<<< HEAD
         $this->enabled = $this->Config->init('checks', 'http_checker', $this->enabled, 'проверка версии HTTP-протокола');
-=======
-        $this->enabled = $this->Config->init('checks', 'http_checker', $this->enabled, '');
->>>>>>> bf153bf80a139e6bad61a33bbf5d57287f4fa76f
         $this->protocols = $this->Config->init('http_checker', 'protocols', ['HTTP/1.0'], 'перечисления протоколов через запятую, HTTP/1.0,HTTP/1.1');
         $this->action = $this->Config->init('http_checker', 'action', $this->action, 'ALLOW - разрешить, CAPTCHA - капча, BLOCK - заблокировать, SKIP - пропустить правило');
         $this->addBlacklistIP = $this->Config->init('http_checker', 'add_blacklist_ip', $this->addBlacklistIP, 'On - добавить ip в черный список');
