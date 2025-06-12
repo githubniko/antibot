3551fabc3e39c49e4b9bd4451f33f0eb725f7c36
Egor NiKO <niko_egor@mail.ru>
2025-06-12 13:43:03 +0300

    Add ipToRange(), validateASN()

--- includes/Utility/Network.class.php
+++ includes/Utility/Network.class.php
@@ -42,7 +42,7 @@ class Network
 
         // Обработка диапазона (192.168.1.1-192.168.1.100)
         if (strpos($range, '-') !== false) {
-            
+
             return self::checkIpRange($ipBinary, $range);
         }
 
@@ -61,7 +61,7 @@ class Network
         }
 
         list($network, $netmask) = explode('/', $cidr, 2);
-        
+
         $networkBinary = self::ipToBinary($network);
         if ($networkBinary === false) {
             return false;
@@ -73,7 +73,7 @@ class Network
 
         $maxNetmask = strlen($networkBinary) * 8;
         $netmask = (int)$netmask;
-        
+
         return $netmask >= 0 && $netmask <= $maxNetmask;
     }
 
@@ -111,7 +111,7 @@ class Network
     }
 
     /**
-     * Проверяет CIDR-диапазон
+     * Проверяет вхождение ip в CIDR-диапазон
      * @param string $ipBinary
      * @param string $cidrRange
      * @return bool
@@ -119,7 +119,7 @@ class Network
     private static function checkCidrRange($ipBinary, $cidrRange)
     {
         list($network, $netmask) = explode('/', $cidrRange, 2);
-        
+
         $networkBinary = self::ipToBinary($network);
         if ($networkBinary === false || strlen($networkBinary) !== strlen($ipBinary)) {
             return false;
@@ -146,7 +146,7 @@ class Network
     private static function checkIpRange($ipBinary, $ipRange)
     {
         list($startIp, $endIp) = explode('-', $ipRange, 2);
-        
+
         $startBinary = self::ipToBinary($startIp);
         $endBinary = self::ipToBinary($endIp);
         if ($startBinary === false || $endBinary === false || strlen($startBinary) !== strlen($ipBinary)) {
@@ -189,5 +189,120 @@ class Network
         }
 
         return str_pad($mask, $bytesLength, "\x00");
+    }
+
+    /**
+     * Возвращает mix-max значения CIDR
+     */
+    public static function ipToRange($ipOrCidr)
+    {
+        if (strpos($ipOrCidr, ':') !== false) {
+            // Обработка IPv6
+            return self::ipv6ToRange($ipOrCidr);
+        } else {
+            // Обработка IPv4
+            return self::ipv4ToRange($ipOrCidr);
+        }
+    }
+
+    /**
+     * Возвращает mix-max значения CIDR для IPv4
+     */
+    private static function ipv4ToRange($ipOrCidr)
+    {
+        if (self::isValidCidr($ipOrCidr) !== false) {
+            list($subnet, $mask) = explode('/', $ipOrCidr);
+            $ip_beg = ip2long($subnet) & (~((1 << (32 - $mask)) - 1));
+            $ip_end = $ip_beg + (1 << (32 - $mask)) - 1;
+        } else {
+            $ip_beg = $ip_end = ip2long($ipOrCidr);
+        }
+
+        return [
+            'is_ipv6' => 0,
+            'beg' => pack('N', $ip_beg),  // 4-байтовое представление
+            'end' => pack('N', $ip_end)
+        ];
+    }
+
+    /**
+     * Возвращает mix-max значения CIDR для IPv6
+     */
+    private static function ipv6ToRange($ipOrCidr)
+    {
+        if (self::isValidCidr($ipOrCidr) !== false) {
+            list($subnet, $mask) = explode('/', $ipOrCidr);
+            $binary = inet_pton($subnet);
+
+            // Вычисляем диапазон для IPv6 CIDR
+            $mask_bits = str_repeat('f', $mask >> 2);
+            switch ($mask % 4) {
+                case 1:
+                    $mask_bits .= '8';
+                    break;
+                case 2:
+                    $mask_bits .= 'c';
+                    break;
+                case 3:
+                    $mask_bits .= 'e';
+                    break;
+            }
+            $mask_bits = str_pad($mask_bits, 32, '0');
+            $mask_bin = pack('H*', $mask_bits);
+
+            $ip_beg = $binary & $mask_bin;
+            $ip_end = $binary | ~$mask_bin;
+        } else {
+            $ip_beg = $ip_end = inet_pton($ipOrCidr);
+        }
+
+        return [
+            'is_ipv6' => 1,
+            'beg' => $ip_beg,
+            'end' => $ip_end
+        ];
+    }
+
+    /**
+     * Валидация номера автономной системы (ASN)
+     * @param string|int $asn Проверяемый ASN (может быть строкой или числом)
+     * @return bool Возвращает true если ASN валиден
+     */
+    public static function validateASN($asn)
+    {
+        // Приводим к строке и удаляем возможные префиксы
+        $asnStr = (string)$asn;
+        $asnStr = strtoupper($asnStr);
+
+        // Удаляем префикс "AS" если есть
+        if (strpos($asnStr, 'AS') === 0) {
+            $asnStr = substr($asnStr, 2);
+        }
+
+        // Проверяем числовой формат
+        if (!ctype_digit($asnStr)) {
+            return false;
+        }
+
+        // Преобразуем в число для проверки диапазонов
+        $asnNumber = (int)$asnStr;
+
+        // Основные диапазоны ASN:
+        // 0 - зарезервирован
+        // 1-64495 - публичные ASN
+        // 64496-64511 - зарезервировано для документации (RFC5398)
+        // 64512-65534 - частные ASN (RFC6996)
+        // 65535 - зарезервирован
+        // 65536-4294967295 - 32-битные ASN (4-байтовые)
+
+        if ($asnNumber === 0 || $asnNumber === 65535) {
+            return false; // Зарезервированные значения
+        }
+
+        if ($asnNumber >= 1 && $asnNumber <= 4294967295) {
+            return true;
+        }
+
+        return false;
     }
 }
