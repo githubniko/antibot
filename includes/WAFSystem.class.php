376784637938ec737820501d4f912e70e11d0391
Egor NiKO <niko_egor@mail.ru>
2025-06-12 14:46:06 +0300

    Скорректирован приоритет проверок

--- includes/WAFSystem.class.php
+++ includes/WAFSystem.class.php
@@ -84,20 +84,81 @@ class WAFSystem
         if ($this->HTTPChecker->enabled)
             $this->Logger->log("Protocol: " . $this->Profile->HttpVersion);
 
+        # БОЛЕЕ ТЯЖЕЛЫЕ ПРОВЕРКИ ДОБАВЛЯЮТСЯ В КОНЕЦ
+        ##### ALLOW #####
 
-        // 1. Проверка URL в белом списке
+        # Проверка куки маркера
+        if ($this->Marker->isValid()) {
+            $this->Logger->log("Tag found");
+            return true;
+        }
+
+        # Пропускаем посетителей с Прямыми заходом
+        if ($this->RefererChecker->enabled) {
+            $this->Logger->log("REF: " . $this->Profile->Referer);
+
+            if ($this->RefererChecker->isDirect($this->Profile->Referer, 'ALLOW')) {
+                $this->Logger->log("DIRECT allowed");
+                return true;
+            }
+            // Пропускаем посетителей с реферером (будут фильтроваться только прямые заходы)
+            if ($this->RefererChecker->isReferer($this->Profile->Referer, 'ALLOW')) {
+                $this->Logger->log("HTTP_REFERER allowed");
+                return true;
+            }
+        }
+
+        # Разрешенные IP
+        if ($this->WhiteListIP->enabled) {
+            if ($this->WhiteListIP->isListed($clientIp)) {
+                $this->Logger->log("IP address found in whitelist: $clientIp");
+                return true;
+            }
+        }
+        
+        # Разрешенные URL
         if ($this->RequestChecker->enabled) {
-            if ($this->RequestChecker->isListed($this->Profile->REQUEST_URI)) {
-                if ($this->RequestChecker->action == 'ALLOW') {
+            if ($this->RequestChecker->action == 'ALLOW') {
+                if ($this->RequestChecker->isListed($this->Profile->REQUEST_URI)) {
                     $this->Logger->log("REQUEST_URI allowed");
                     return true;
-                } else {
-                    $this->Logger->log("REQUEST_URI skipped");
                 }
             }
         }
 
-        // 2. Проверка IP в черном списке
+        # Разрешенные User-Agent
+        if ($this->UserAgentChecker->enabled) {
+            if ($this->UserAgentChecker->action == 'ALLOW') {
+                if ($this->UserAgentChecker->isListed($this->Profile->UserAgent)) {
+                    $this->Logger->log("User-Agent allowed");
+                    return true;
+                }
+            }
+        }
+
+        # Разрешенные поисковые боты
+        if ($this->IndexBot->enabled) {
+            if ($this->IndexBot->Checking($clientIp)) {
+                $this->Logger->log("Indexing robot");
+                return true;
+            }
+        }
+
+        ##### BLOCK #####
+
+        # Блокировка IPv6
+        if ($this->BlackLiskIP->enabled) {
+            if ($this->BlackLiskIP->ipv6 == 'BLOCK') {
+                if ($this->BlackLiskIP->isIPv6($this->Profile->IP)) {
+                    $this->Logger->log("IPv6 blocked");
+                    $this->Template->showBlockPage();
+                }
+            }
+        }
+
+        # Блокировка IP
+        # ПРАВИЛО НЕ БУДЕТ СРАБАТЫВАТЬ, ЕСЛИ У ПОСЕТИТЕЛЯ УСТАНОВЛЕНА МЕТКА
+        # НУЖНО РЕАЛИЗОВАТЬ МЕХАНИЗМ СБРОСА МЕТКИ ДЛЯ КОНКРЕТНОГО ПОСЕТИТЕЛЯ
         if ($this->BlackLiskIP->enabled) {
             if ($this->BlackLiskIP->isListed($clientIp)) {
                 $this->Logger->log("IP address found on blacklist: $clientIp");
@@ -105,7 +166,28 @@ class WAFSystem
             }
         }
 
-        // Проверка ASN
+        # Проверка User-Agent
+        if ($this->UserAgentChecker->enabled) {
+            // Валидность User_Agent
+            if (!$this->UserAgentChecker->isValid($this->Profile->UserAgent)) {
+                $this->Template->showBlockPage();
+            }
+        }
+
+        # Блокировка HTTP протоколов
+        if ($this->HTTPChecker->enabled) {
+            if ($this->HTTPChecker->action == 'BLOCK') {
+                if ($this->HTTPChecker->Checking($this->Profile->HttpVersion)) {
+                    $this->Logger->log("Version HTTP blocked");
+                    if ($this->HTTPChecker->addBlacklistIP) {
+                        $this->BlackLiskIP->add($clientIp, $this->Profile->HttpVersion);
+                    }
+                    $this->Template->showBlockPage();
+                }
+            }
+        }
+
+        # Блокировка ASN
         if ($this->ASNChecker->enabled) {
             if ($this->ASNChecker->action == 'BLOCK') {
                 if ($this->ASNChecker->Checking($this->Profile->IP)) {
@@ -115,72 +197,12 @@ class WAFSystem
             }
         }
 
-        // 3. Проверка куки маркера
-        if ($this->Marker->isValid()) {
-            $this->Logger->log("Tag found");
-            return true;
-        }
-
-        // 4. Проверка IP в белом списке
-        if ($this->WhiteListIP->enabled) {
-            if ($this->WhiteListIP->isListed($clientIp)) {
-                $this->Logger->log("IP address found in whitelist: $clientIp");
-                return true;
-            }
-        }
-
-        // Пропускаем посетителей с Прямым заходом
-        if ($this->RefererChecker->enabled) {
-            $this->Logger->log("REF: " . $this->Profile->Referer);
-
-            if ($this->RefererChecker->isDirect($this->Profile->Referer, 'ALLOW')) {
-                $this->Logger->log("DIRECT allowed");
-                $this->Marker->set();
-                return true;
-            }
-            // Пропускаем посетителей с реферером (будут фильтроваться только прямые заходы)
-            if ($this->RefererChecker->isReferer($this->Profile->Referer, 'ALLOW')) {
-                $this->Logger->log("HTTP_REFERER allowed");
-                $this->Marker->set();
-                return true;
-            }
-        }
-
-        // 5. Проверка User-Agent
-        if ($this->UserAgentChecker->enabled) {
-            // Валидность User_Agent
-            if (!$this->UserAgentChecker->isValid($this->Profile->UserAgent)) {
-                return false;
-            }
-
-            // Пропускаем исключенные User-Agent
-            if ($this->UserAgentChecker->isListed($this->Profile->UserAgent)) {
-                if ($this->UserAgentChecker->action == 'ALLOW') {
-                    $this->Logger->log("User-Agent allowed");
-                    return true;
-                }
-            }
-        }
-
-        // 7. Проверка поисковых ботов
-        if ($this->IndexBot->enabled) {
-            if ($this->IndexBot->Checking($clientIp)) {
-                $this->Logger->log("Indexing robot");
-                return true;
-            }
-        }
-
-        // 9. Проверка протокола
-        if ($this->HTTPChecker->enabled) {
-            if ($this->HTTPChecker->Checking($this->Profile->HttpVersion)) {
-                if ($this->HTTPChecker->action == 'BLOCK') {
-                    $this->Logger->log("Version HTTP blocked");
-                    if ($this->HTTPChecker->addBlacklistIP) {
-                        $this->BlackLiskIP->add($clientIp, $this->Profile->HttpVersion);
-                    }
+        # Блокировка TOR
+        if ($this->TorChecker->enabled) {
+            if ($this->TorChecker->action == 'BLOCK') {
+                if ($this->TorChecker->isTor($this->Profile->IP)) {
+                    $this->Logger->log("TOR blocked");
                     $this->Template->showBlockPage();
-                } else if ($this->HTTPChecker->action == 'SKIP') {
-                    $this->Logger->log("Version HTTP skipped");
                 }
             }
         }
@@ -191,9 +213,9 @@ class WAFSystem
     public function isAllowed2()
     {
         $Api = Api::getInstance($this);
-        new SysUpdate($this->Config, $this->Logger); // Обновляем систему
+        new SysUpdate($this->Config, $this->Logger); // Обновляем систему пока проходит проверка на роботность
 
-        # Важен приоритет проверки
+        # Блокировка плохих запросов
         if (!$Api->isPost()) {
             $this->Logger->log("Not a POST request");
             $this->BlackLiskIP->add($this->Profile->IP, 'Not a POST request');
@@ -202,12 +224,15 @@ class WAFSystem
 
         $data = $Api->getData();
 
+        # Блокировка, eсли не удалось получить FingerPrint
         if (!isset($data['fingerPrint']) || empty($data['fingerPrint'])) {
             $this->Logger->log("Not FingerPrint");
             $Api->endJSON('block');
         }
-        $this->Profile->FingerPrint = $data['fingerPrint'];
 
+        $this->Profile->FingerPrint = $data['fingerPrint']; // дополняем профиль посетителя FP
+
+        # Вывод в лог значения FP
         if ($this->FingerPrint->enabled)
             $this->Logger->log("FP:  " . $this->Profile->FingerPrint);
 
@@ -217,9 +242,8 @@ class WAFSystem
             $Api->endJSON(''); // возможно тут нужно добавлять пользователя в черный список
         }
 
-        /* 
-        * ALLOW
-        **/
+        ##### ALLOW #####
+
         # Запрос на установку метки
         if ($data['func'] == 'set-marker' && $Api->isHiddenValue()) {
             $this->Logger->log("Successfully passed the captcha");
@@ -227,79 +251,37 @@ class WAFSystem
             $Api->endJSON('allow');
         }
 
-        /* 
-        * BLOCK
-        **/
+        ##### BLOCK #####
+
+        # Блокировка мобильных девайсов
+        if ($this->MobileChecker->enabled) {
+            if ($this->MobileChecker->action == 'BLOCK') {
+                if ($this->MobileChecker->Checking($this->Profile->isMobile, $data['screenWidth'], $data['pixelRatio'])) {
+                    $this->Logger->log("Mobile device blocked");
+                    $Api->endJSON('block');
+                }
+            }
+        }
 
         # Блокировка по FingerPrint
         if ($this->FingerPrint->enabled) {
-            if ($this->FingerPrint->Checking($this->Profile->FingerPrint)) {
-                if ($this->FingerPrint->action == 'BLOCK') {
+            if ($this->FingerPrint->action == 'BLOCK') {
+                if ($this->FingerPrint->Checking($this->Profile->FingerPrint)) {
                     $this->Logger->log("FingerPrint blocked");
                     if ($this->FingerPrint->addBlacklistIP) {
                         $this->BlackLiskIP->add($this->Profile->IP, 'FP ' . $this->Profile->FingerPrint);
                     }
                     $Api->endJSON('block');
-                } else {
-                    $this->Logger->log("FingerPrint skipped");
                 }
             }
         }
 
-        # Проверка для iframe
+        # Блокировка преходов в iframe
         if ($this->IFrameChecker->enabled) {
-            if ($this->IFrameChecker->Checking($data['mainFrame'])) {
-                if ($this->IFrameChecker->action == 'BLOCK') {
+            if ($this->IFrameChecker->action == 'BLOCK') {
+                if ($this->IFrameChecker->Checking($data['mainFrame'])) {
                     $this->Logger->log("IFrame blocked");
                     $Api->endJSON('block');
-                } elseif ($this->IFrameChecker->action == 'CAPTCHA') {
-                    $this->Logger->log("IFrame captcha");
-                    $Api->endJSON('captcha');
-                } else {
-                    $this->Logger->log("IFrame skipped");
-                }
-            }
-        }
-
-        /* 
-        * BLOCK OR CAPTCHA
-        **/
-
-        # Тор IP
-        if ($this->TorChecker->enabled) {
-            if ($this->TorChecker->isTor($this->Profile->IP)) {
-                $this->Logger->log("IP is TOR exit node");
-                if ($this->TorChecker->action == 'BLOCK') {
-                    $this->BlackLiskIP->add($this->Profile->IP, 'tor');
-                    $Api->endJSON('block');
-                } elseif ($this->TorChecker->action == 'CAPTCHA') {
-                    $Api->endJSON('captcha');
-                }
-            }
-        }
-
-        # Проверка ASN
-        if ($this->ASNChecker->enabled) {
-            if ($this->ASNChecker->action == 'CAPTCHA') {
-                if ($this->ASNChecker->Checking($this->Profile->IP)) {
-                    $this->Logger->log("Show captcha for ASN");
-                    $Api->endJSON('captcha');
-                }
-            } elseif ($this->ASNChecker->action == 'SKIP') {
-                $this->Logger->log("ASN skipped");
-            }
-        }
-
-        if ($this->BlackLiskIP->enabled) {
-            if ($this->BlackLiskIP->isIPv6($this->Profile->IP)) {
-                if ($this->BlackLiskIP->ipv6 == 'BLOCK') {
-                    $this->Logger->log("IPv6 blocked");
-                    $Api->endJSON('block');
-                } elseif ($this->BlackLiskIP->ipv6 == 'CAPTCHA') {
-                    $this->Logger->log("IPv6 captcha");
-                    $Api->endJSON('captcha');
-                } else {
-                    $this->Logger->log("IPv6 skipped");
                 }
             }
         }
@@ -308,30 +290,17 @@ class WAFSystem
         * CAPTCHA
         **/
 
-        # Показ капчи для Протоколов
-        if ($this->HTTPChecker->enabled) {
-            if ($this->HTTPChecker->Checking($this->Profile->HttpVersion)) {
-                if ($this->HTTPChecker->action == 'CAPTCHA') {
-                    $this->Logger->log("Show captcha for protocol: " . $this->Profile->HttpVersion);
+        # Проверка IPv6
+        if ($this->BlackLiskIP->enabled) {
+            if ($this->BlackLiskIP->ipv6 == 'CAPTCHA') {
+                if ($this->BlackLiskIP->isIPv6($this->Profile->IP)) {
+                    $this->Logger->log("IPv6 captcha");
                     $Api->endJSON('captcha');
                 }
             }
         }
 
-        # Проверка для мобильных девайсов
-        if ($this->MobileChecker->enabled) {
-            if ($this->MobileChecker->Checking($this->Profile->isMobile, $data['screenWidth'], $data['pixelRatio'])) {
-                if ($this->MobileChecker->action == 'CAPTCHA') {
-                    $this->Logger->log("Show captcha for Mobile device");
-                    $Api->endJSON('captcha');
-                } elseif ($this->MobileChecker->action == 'BLOCK') {
-                    $this->Logger->log("Mobile device blocked");
-                    $Api->endJSON('block');
-                } elseif ($this->MobileChecker->action == 'SKIP') {
-                    $this->Logger->log("Mobile device skipped");
-                }
-            }
-        }
+        
 
         # Показ капчи для Прямых заходов
         if ($this->RefererChecker->enabled) {
@@ -346,7 +315,55 @@ class WAFSystem
             }
         }
 
+        # Проверка для мобильных девайсов
+        if ($this->MobileChecker->enabled) {
+            if ($this->MobileChecker->action == 'CAPTCHA') {
+                if ($this->MobileChecker->Checking($this->Profile->isMobile, $data['screenWidth'], $data['pixelRatio'])) {
+                    $this->Logger->log("Show captcha for Mobile device");
+                    $Api->endJSON('captcha');
+                }
+            }
+        }
 
+        # Проверка для iframe
+        if ($this->IFrameChecker->enabled) {
+            if ($this->IFrameChecker->action == 'CAPTCHA') {
+                if ($this->IFrameChecker->Checking($data['mainFrame'])) {
+                    $this->Logger->log("IFrame captcha");
+                    $Api->endJSON('captcha');
+                }
+            }
+        }
+
+        # Показ капчи для Протоколов
+        if ($this->HTTPChecker->enabled) {
+            if ($this->HTTPChecker->action == 'CAPTCHA') {
+                if ($this->HTTPChecker->Checking($this->Profile->HttpVersion)) {
+                    $this->Logger->log("Show captcha for protocol: " . $this->Profile->HttpVersion);
+                    $Api->endJSON('captcha');
+                }
+            }
+        }
+
+        # Проверка ASN
+        if ($this->ASNChecker->enabled) {
+            if ($this->ASNChecker->action == 'CAPTCHA') {
+                if ($this->ASNChecker->Checking($this->Profile->IP)) {
+                    $this->Logger->log("Show captcha for ASN");
+                    $Api->endJSON('captcha');
+                }
+            }
+        }
+
+        # Тор IP
+        if ($this->TorChecker->enabled) {
+            if ($this->TorChecker->action == 'CAPTCHA') {
+                if ($this->TorChecker->isTor($this->Profile->IP)) {
+                    $this->Logger->log("IP is TOR captcha");
+                    $Api->endJSON('captcha');
+                }
+            }
+        }
 
         $this->Logger->log("Passed all filters");
         $this->Marker->set();
